95ddd50f513f9bf43d56b55f86549470ec26b107
Rafael H Lisot <rafael.lisot@unoesc.edu.br>
2020-09-15 20:35:04 -0300

    Projeto Clone NetLix (Pagina inicial)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import Tmdb from './Tmdb';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import MovieRow from './components/MovieRow';
+import FeaturedMovie from './components/FeaturedMovie';
+import Header from './components/Header';
+
+export default () => {
+
+    const [movieList, setMovieList] = useState([]);
+    const [featuredData, setFeaturedData] = useState(null);
+    const [blackHeader, setBlackHeader] = useState(false);
+    
+    useEffect(() => {
+        const loadAll = async () => {
+            let list = await Tmdb.getHomeList();
+            setMovieList(list);
+
+            let originals = list.filter(i => i.slug === 'originals');
+            let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+            let chosen = originals[0].items.results[randomChosen];
+            let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+            setFeaturedData(chosenInfo); 
+        }
+
+        loadAll();
+    }, []);
+
+    useEffect(() => {
+        const scrollListner = () => {
+            if (window.scrollY > 10) {
+                setBlackHeader(true);
+            } else {
+                setBlackHeader(false);
+            }
+        }
+
+        window.addEventListener('scroll', scrollListner);
+
+        return () => {
+            window.removeEventListener('scroll', scrollListner);
+        }
+    }, []);
+
+    return (
+        <div classNmae="page">
+            <Header black={blackHeader} />
+            {featuredData &&
+                <FeaturedMovie item={featuredData} />
+            }
+            <section className="lists">
+                {movieList.map((item, key) => (
+                    <MovieRow key={key} title={item.title} items={item.items} />
+                ))}
+            </section>
+
+            <footer>
+                Projeto de treino feito pela equipe da <span>RN Softwares</span><br/>
+                Direitos de imagem para NetFlix<br/>
+                Dados pegos do site themoviedb.org
+            </footer>
+
+            {movieList.length <= 0 &&
+                <div className="loading">
+                    <img src="https://www.filmelier.com/pt/br/news/wp-content/uploads/2020/03/netflix-loading.gif" alt="Carregando" />
+                </div>
+            }
+        </div>
+    )
 }
-
-export default App;
